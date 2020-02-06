448fe9979ffe9f64d91aaef94c5a1320d2602c88
rezaraa <rabiei_ahmadreza@yahoo.com>
2020-02-06 19:55:22 +0330

    extension function and Lambda

--- app/src/main/java/com/rezara/advancecoursekotlin/MainActivity.kt
+++ app/src/main/java/com/rezara/advancecoursekotlin/MainActivity.kt
@@ -5,27 +5,32 @@ import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() ,Interface{
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val list = arrayListOf( "Name" , "Family" , "Age" )
+        var clickListener: (String)->Unit={
+           when(it){
+           "Name" -> Toast.makeText(this, "Reza", Toast.LENGTH_SHORT).show()
+           "Family" -> Toast.makeText(this, "rabiei", Toast.LENGTH_SHORT).show()
+           "Age" -> Toast.makeText(this, "23", Toast.LENGTH_SHORT).show()
 
-        val adapter = Recycleradapter(list, this)
+           }
+        }
+        val adapter = Recycleradapter(list,clickListener)
         recycler.adapter = adapter
 
-    }
 
-    override fun onclick(name: String) {
-        when (name) {
-            "Name" -> Toast.makeText(this, "Reza", Toast.LENGTH_SHORT).show()
-            "Family" -> Toast.makeText(this, "rabiei", Toast.LENGTH_SHORT).show()
-            "Age" -> Toast.makeText(this, "23", Toast.LENGTH_SHORT).show()
+//    override fun onclick(name: String) {
+//        when (name) {
+//            "Name" -> Toast.makeText(this, "Reza", Toast.LENGTH_SHORT).show()
+//            "Family" -> Toast.makeText(this, "rabiei", Toast.LENGTH_SHORT).show()
+//            "Age" -> Toast.makeText(this, "23", Toast.LENGTH_SHORT).show()
 
         }
     }
 
 
 
-}
